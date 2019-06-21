781c06c716de9d4d6f25376b08fb87883dd07d7d
Corneliu <corneliu.gaina@gmail.com>
2019-06-21 19:02:50 +0200

    setting the ninja's props - lesson 13 -

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React app!</h1>
+        <p>Welcome :D</p>
+        <Ninjas name="Ryu" age="25" belt="black"/>
+        <Ninjas name="Yoshi" age="30" belt="grey"/>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App; 
