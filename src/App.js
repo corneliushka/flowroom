410309c82a2e512dd0b6ffe71dcf3fa8e9406499
Corneliu <corneliu.gaina@gmail.com>
2019-07-22 17:07:38 +0200

    Redirect added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter, Route, Switch} from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+import { BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -15,11 +15,12 @@ class App extends Component {
           <h3 className="brand-logo center cover">FlowRoom</h3>
           <Navbar />
           <Switch>
-            <Route exact path='/flowroom/' component={Home} />
-            <Route path='/flowroom/home' component={Home} />
+            {/* path to delete after development finished */}
+            <Route path='/flowroom/' component={Home} />
             <Route path='/flowroom/about' component={About} />
             <Route path='/flowroom/contact' component={Contact} />
-            <Route path="/flowroom/:post_id" component={Post} /> {/* we'll get access to the extra information in the object automatically */}       
+            <Route path="/flowroom/:post_id" component={Post} /> {/* we'll get access to the extra information in the object automatically */}
+            <Route render={() => <Redirect to={"/flowroom"}/>} />       
           </Switch>
         </div>
       </BrowserRouter>
